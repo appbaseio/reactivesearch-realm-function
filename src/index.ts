83997db82206192a6771feaac4dae24ef8382e83
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-08-17 16:06:20 +0530

    feat: add search target query

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export type ConfigType = {
-	url: string;
-};
+import { getSearchQuery } from './targets/search';
+import { ConfigType, RSQuery } from './types';
 
 export class Realm {
 	config: ConfigType;
@@ -8,4 +7,24 @@ export class Realm {
 	constructor(config: ConfigType) {
 		this.config = config;
 	}
+
+	// TODO define type for mongo query
+	query = (data: [RSQuery]) => {
+		// TODO decide query format from set of multiple queries
+
+		// pipeline used by mongodb aggregation
+		// TODO set type as per mongo query type
+		const aggPipeline: any = [];
+
+		data.forEach((item) => {
+			if (item.type === `search`) {
+				aggPipeline.push(getSearchQuery(item));
+			}
+
+			aggPipeline.push({ $limit: item.size || 10 });
+			aggPipeline.push({ $skip: item.from || 0 });
+		});
+
+		return aggPipeline;
+	};
 }
