2fdda413b2de9a77bb55993770dfac68234568e8
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2021-09-08 12:23:25 +0530

    Reverting changes

--- __test__/targets/common.test.ts
+++ __test__/targets/common.test.ts
@@ -178,7 +178,7 @@ test('getIncludeExcludeFields when includeFields, excludeFields contains some co
 	});
 	const expected = {
 		$project: {
-			test: 1,
+			test: 0,
 			test1: 1,
 			test2: 1,
 			highlights: { $meta: 'searchHighlights' },
