b17a5d619ae1e68d13e1a3ee15c90f2a85e70804
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2021-09-13 21:16:50 +0530

    Error handling

--- rs-cli.js
+++ rs-cli.js
@@ -36,46 +36,52 @@ const options = yargs
 const { api_key, public_api_key, app_id, app_authentication } = options;
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
+let webhookEndpoint;
 console.log('\nDeploying webhook\n');
-bar.start(5, 0);
-execSync(
-	`./node_modules/mongodb-realm-cli/realm-cli login --api-key=${api_key} --private-api-key=${public_api_key}`,
-);
-bar.update(1);
-execSync(
-	`rm -rf realm-app && ./node_modules/mongodb-realm-cli/realm-cli pull --local=./realm-app  --remote=${app_id}`,
-);
-bar.update(2);
-execSync(
-	`node build --name=${WEBHOOK_NAME}` +
-		(app_authentication ? ` --app-authentication=${app_authentication}` : ''),
-);
-bar.update(3);
-execSync(
-	`./node_modules/mongodb-realm-cli/realm-cli push --local=./realm-app --yes`,
-);
-bar.update(4);
-const appDescription = JSON.parse(
+try {
+	bar.start(5, 0);
 	execSync(
-		`./node_modules/mongodb-realm-cli/realm-cli app describe --app=${app_id}`,
-	)
-		.toString()
-		.replace('App description', ''),
-);
-const { http_endpoints } = appDescription;
-let webhookEndpoint = null;
-http_endpoints.map((endpoint) =>
-	endpoint.webhooks.map((hook) => {
-		if (hook.name === WEBHOOK_NAME) {
-			webhookEndpoint = hook.url;
-		}
-	}),
-);
-bar.update(5);
-bar.stop();
-console.info('\nSuccessfully deployed webhook.');
+		`./node_modules/mongodb-realm-cli/realm-cli login --api-key=${api_key} --private-api-key=${public_api_key}`,
+	);
+	bar.update(1);
+	execSync(
+		`rm -rf realm-app && ./node_modules/mongodb-realm-cli/realm-cli pull --local=./realm-app  --remote=${app_id}`,
+	);
+	bar.update(2);
+	execSync(
+		`node build --name=${WEBHOOK_NAME}` +
+			(app_authentication ? ` --app-authentication=${app_authentication}` : ''),
+	);
+	bar.update(3);
+	execSync(
+		`./node_modules/mongodb-realm-cli/realm-cli push --local=./realm-app --yes`,
+	);
+	bar.update(4);
+	const appDescription = JSON.parse(
+		execSync(
+			`./node_modules/mongodb-realm-cli/realm-cli app describe --app=${app_id}`,
+		)
+			.toString()
+			.replace('App description', ''),
+	);
+	let { http_endpoints } = appDescription;
+	http_endpoints.map((endpoint) =>
+		endpoint.webhooks.map((hook) => {
+			if (hook.name === WEBHOOK_NAME) {
+				webhookEndpoint = hook.url;
+			}
+		}),
+	);
+	bar.update(5);
+	bar.stop();
+	console.info('\n Successfully deployed webhook.');
+} catch (error) {
+	console.log(error);
+	console.error('\n Failed to deploy webhook');
+	bar.stop();
+}
 
-if (http_endpoints) {
+if (webhookEndpoint) {
 	console.info(`\n Deployed webhook endpoint : ${webhookEndpoint}`);
 	console.log(
 		`\n You can make a test request to this webhook using this curl command \n`,
@@ -97,5 +103,5 @@ if (http_endpoints) {
         ${webhookEndpoint}
      `);
 } else {
-	console.error("Something went wrong. We aren't able to find endpoint");
+	console.error("\n Something went wrong. We aren't able to find endpoint");
 }
