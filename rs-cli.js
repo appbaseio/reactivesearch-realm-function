5f722083b4b75b8813eaade9f046d9dfd889c53d
Deepak Aggarwal <deepak.aggarwal9@gmail.com>
2021-09-14 14:57:49 +0530

    Updated rs-cli

--- rs-cli.js
+++ rs-cli.js
@@ -31,32 +31,39 @@ const options = yargs
 		describe: 'Application Authentication',
 		type: 'string',
 		demandOption: false,
+	})
+	.option('-v', {
+		alias: 'verbose',
+		describe: 'Verbose',
+		demandOption: false,
 	}).argv;
 
-const { api_key, private_api_key, app_id, app_authentication } = options;
+const { api_key, private_api_key, app_id, app_authentication, verbose } =
+	options;
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 let webhookEndpoint;
 console.log('\nDeploying ReactiveSearch API as a Realm function\n');
+let step = -1;
 try {
-	bar.start(5, 0);
+	bar.start(5, ++step);
 	execSync(
 		`./node_modules/mongodb-realm-cli/realm-cli login --api-key=${api_key} --private-api-key=${private_api_key}`,
 	);
-	bar.update(1);
+	bar.update(++step);
 	execSync(
 		`rm -rf realm-app && ./node_modules/mongodb-realm-cli/realm-cli pull --local=./realm-app  --remote=${app_id}`,
 	);
-	bar.update(2);
+	bar.update(++step);
 	execSync(
 		`node build --name=${WEBHOOK_NAME}` +
 			(app_authentication ? ` --app-authentication=${app_authentication}` : ''),
 	);
-	bar.update(3);
+	bar.update(++step);
 	execSync(
 		`./node_modules/mongodb-realm-cli/realm-cli push --local=./realm-app --yes`,
 	);
-	bar.update(4);
+	bar.update(++step);
 	const appDescription = JSON.parse(
 		execSync(
 			`./node_modules/mongodb-realm-cli/realm-cli app describe --app=${app_id}`,
@@ -72,11 +79,42 @@ try {
 			}
 		}),
 	);
-	bar.update(5);
+	bar.update(++step);
 	bar.stop();
 	console.info('\n Successfully deployed webhook.');
 } catch (error) {
-	console.log(error);
+	switch (step) {
+		case 0:
+			console.error(
+				'\n Deployment Failed: Please check you public and private api keys',
+			);
+			break;
+		case 1:
+			console.error(
+				'\n Deployment Failed: Unable to pull application configuration. Please check your application id.',
+			);
+			break;
+		case 2:
+			console.error(
+				'\n Deployment Failed: Please make sure to run yarn install or npm install',
+			);
+			break;
+		case 3:
+			console.error(
+				'\n Deployment Failed: Unable to push application configuration. Please check your application id.',
+			);
+			break;
+		case 4:
+			console.error(
+				'\n Deployment Failed: Unable to find endpoint of http webhook',
+			);
+			break;
+	}
+	if (verbose) {
+		console.log('\n');
+		console.log(error.toString());
+	}
+
 	console.error('\n Failed to deploy webhook');
 	bar.stop();
 }
